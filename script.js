09fd87de94a1faa4abfdfec0f20457c5cb115c6a
Doug McGillivray <mcgidoug@gmail.com>
2024-12-17 12:31:08 -0600

    added collapsible navbar functionality

--- script.js
+++ script.js
@@ -15,3 +15,8 @@ function navigateTo(page) {
     console.error("Invalid page:", page);
   }
 }
+
+document.getElementById("toggle-button").addEventListener("click", function () {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active"); // Toggle the 'active' class
+});
